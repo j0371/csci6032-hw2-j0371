045c44e3bfc886dc49e7d9d1a79f30d4a94324cf
Marcus Higgins <halo2305@gmail.com>
2026-09-14 22:33:40 -0500

    Committing to pull from Laptop

--- StartNotebook.bat
+++ StartNotebook.bat
@@ -2,8 +2,6 @@
 cd /d "%~dp0"
 
 if not exist .venv py -3 -m venv .venv
-call .venv\Scripts\activate.bat
 .venv\Scripts\python.exe -m pip install --upgrade -r requirements.txt
-
-if errorlevel 1 exit /b 1
+call .venv\Scripts\activate.bat
 .venv\Scripts\python.exe -m notebook
